ed686016f5dde8683ed2951af1515474d68cf566
Natthawut Haematulin <natthawut1991@gmail.com>
2021-02-01 23:03:08 +0700

    feat: create NoteList, NoteDetail, Search, Account fragments

---
+++ app/src/main/java/com/example/app/AccountViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.app
+
+import androidx.lifecycle.ViewModel
+
+class AccountViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/app/NoteListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.app
+
+import androidx.lifecycle.ViewModel
+
+class NoteListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
