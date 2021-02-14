976793937b55686430507ddde6dfa7d458067415
Natthawut Haematulin <natthawut1991@gmail.com>
2021-02-14 22:21:45 +0700

    Setup Hilt Dependency Injection (#8)
    * feat: integrate Android Hilt
    
    * feat: setup Hilt to ViewModel
    
    * fix: fix integrating Hilt in Module
    
    * docs: mark DI to done

--- app/src/main/java/com/example/app/NoteListViewModel.kt
+++ app/src/main/java/com/example/app/NoteListViewModel.kt
@@ -3,12 +3,14 @@ package com.example.app
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.example.app.data.NoteRepository
 import com.example.app.data.NoteRepositoryImp
 import com.example.app.data.models.Note
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class NoteListViewModel(
-    private val noteRepository: NoteRepository = NoteRepositoryImp()
+@HiltViewModel
+class NoteListViewModel @Inject constructor(
+    private val noteRepository: NoteRepositoryImp,
 ) : ViewModel() {
 
     val notes: LiveData<List<Note>> = noteRepository.getNoteList().asLiveData()
