f2959193e2475a8c8916109d7f0703b0ad31e1a1
Natthawut Haematulin <natthawut1991@gmail.com>
2021-02-14 08:50:26 +0700

    feat: integrate Android Hilt

---
+++ app/src/main/java/com/example/app/NotesApplication.kt
@@ -0,0 +1,9 @@
+package com.example.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class NotesApplication : Application() {
+}
---
+++ app/src/main/java/com/example/app/di/NetworkModule.kt
@@ -0,0 +1,37 @@
+package com.example.app.di
+
+import com.example.app.data.network.NoteApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+        return Retrofit.Builder()
+            .client(client)
+            .baseUrl("https://blooming-falls-95246.herokuapp.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun provideNoteApi(retrofit: Retrofit): NoteApi {
+        return retrofit.create(NoteApi::class.java)
+    }
+}
