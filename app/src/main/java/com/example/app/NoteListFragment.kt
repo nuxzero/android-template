1d2003950c4b2fdce8ab1a65aa28b4cc03e32692
Natthawut Haematulin <natthawut1991@gmail.com>
2021-02-19 22:48:28 +0700

    fix: hide home up button in account fragmentt

--- app/src/main/java/com/example/app/NoteListFragment.kt
+++ app/src/main/java/com/example/app/NoteListFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -33,8 +32,8 @@ class NoteListFragment : Fragment() {
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         (activity as? AppCompatActivity)?.setSupportActionBar(binding.toolbar)
         NavigationUI.setupWithNavController(binding.toolbar, findNavController())
@@ -49,7 +48,7 @@ class NoteListFragment : Fragment() {
         binding.noteList.layoutManager = LinearLayoutManager(requireContext())
         binding.noteList.adapter = adapter
 
-        viewModel.notes.observe(viewLifecycleOwner, Observer { notes ->
+        viewModel.notes.observe(viewLifecycleOwner, { notes ->
             adapter.setNotes(notes)
         })
     }
