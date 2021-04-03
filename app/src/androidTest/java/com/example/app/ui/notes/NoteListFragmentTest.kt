a627f3e0417f626c7ef1a1a81a94c91c4002bcb3
Natthawut Haematulin <natthawut1991@gmail.com>
2021-04-03 23:03:57 +0700

    test: set navController to fragment

--- app/src/androidTest/java/com/example/app/ui/notes/NoteListFragmentTest.kt
+++ app/src/androidTest/java/com/example/app/ui/notes/NoteListFragmentTest.kt
@@ -1,23 +1,22 @@
 package com.example.app.ui.notes
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import com.example.app.R
 import com.example.app.data.models.Note
 import com.example.app.util.ViewModelFactory
 import com.example.app.util.onAttached
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.`when`
@@ -25,31 +24,37 @@ import org.mockito.Mockito.mock
 
 @RunWith(AndroidJUnit4::class)
 class NoteListFragmentTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
     private lateinit var navHostController: TestNavHostController
     private val mockViewModelFactory = mock(ViewModelFactory::class.java)
     private val mockViewModel = mock(NoteListViewModel::class.java)
     private val notesLiveData = MutableLiveData<List<Note>>()
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
 
     @Before
     fun setUp() {
         `when`(mockViewModelFactory.create(NoteListViewModel::class.java)).thenReturn(mockViewModel)
         `when`(mockViewModel.notes).thenReturn(notesLiveData)
 
-        val scenario = launchFragmentInContainer(null, R.style.Theme_Templateapp) {
+        // Set test NavHostController
+        navHostController = TestNavHostController(context).apply {
+            setGraph(R.navigation.nav_graph)
+            setCurrentDestination(R.id.note_list_fragment)
+        }
+
+        launchFragmentInContainer(themeResId = R.style.Theme_Templateapp) {
             NoteListFragment().apply {
                 onAttached {
                     viewModelFactory = mockViewModelFactory
                 }
-            }
-        }
 
-        // Set test NavHostController
-        navHostController = TestNavHostController(ApplicationProvider.getApplicationContext()).apply {
-            setGraph(R.navigation.nav_graph)
-            setCurrentDestination(R.id.note_list_fragment)
-        }
-        scenario.onFragment { fragment ->
-            Navigation.setViewNavController(fragment.requireView(), navHostController)
+                viewLifecycleOwnerLiveData.observeForever { viewLifecycleOwner ->
+                    if (viewLifecycleOwner != null) {
+                        Navigation.setViewNavController(requireView(), navHostController)
+                    }
+                }
+            }
         }
     }
 
