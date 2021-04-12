a37c3d7c71d0fabe41d2734a016f9e29bccea7b4
Natthawut Haematulin <natthawut1991@gmail.com>
2021-04-12 15:53:32 +0700

    feat: convert xml layout to composable

--- app/src/main/java/com/example/app/ui/note_detail/NoteDetailFragment.kt
+++ app/src/main/java/com/example/app/ui/note_detail/NoteDetailFragment.kt
@@ -6,15 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.NavigationUI
 import com.example.app.R
+import com.example.app.data.models.Note
 import com.example.app.databinding.NoteDetailFragmentBinding
+import com.example.app.ui.theme.AppTheme
 import com.example.app.util.BaseFragment
 import com.example.app.util.themeColor
+import com.google.accompanist.glide.GlideImage
 import com.google.android.material.transition.MaterialContainerTransform
+import java.text.DateFormat
+import java.util.Date
 
 class NoteDetailFragment : BaseFragment() {
 
@@ -23,7 +48,13 @@ class NoteDetailFragment : BaseFragment() {
     private val viewModel: NoteDetailViewModel by viewModels { viewModelFactory }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        binding = NoteDetailFragmentBinding.inflate(inflater, container, false)
+        binding = NoteDetailFragmentBinding.inflate(inflater, container, false).apply {
+            composeView.setContent {
+                AppTheme {
+                    NoteDetailScreen(viewModel)
+                }
+            }
+        }
         return binding.root
     }
 
@@ -46,5 +77,79 @@ class NoteDetailFragment : BaseFragment() {
         viewModel.note.observe(viewLifecycleOwner, { note ->
             binding.note = note
         })
+    }
+}
+
+@Composable
+fun NoteDetailScreen(viewModel: NoteDetailViewModel) {
+    val note by viewModel.note.observeAsState()
+    note?.let { NoteDetailContent(it) }
+}
+
+@Composable
+fun NoteDetailContent(note: Note) {
+    Scaffold(
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Text(
+                    note.title,
+                    color = MaterialTheme.colors.primary,
+                    style = MaterialTheme.typography.h5,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        note.author,
+                        color = MaterialTheme.colors.primary,
+                        style = MaterialTheme.typography.body2,
+                        modifier = Modifier.alignByBaseline(),
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        DateFormat.getDateInstance().format(note.createdAt),
+                        color = MaterialTheme.colors.primary,
+                        style = MaterialTheme.typography.overline,
+                        modifier = Modifier.alignByBaseline(),
+                    )
+                }
+                Spacer(modifier = Modifier.height(24.dp))
+                GlideImage(
+                    data = note.image,
+                    contentDescription = null,
+                    fadeIn = true,
+                    modifier = Modifier
+                        .aspectRatio(1.56f),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                Text(
+                    note.note,
+                    color = MaterialTheme.colors.primary,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun NoteDetailContentPreview() {
+    val note = Note(
+        id = 1,
+        title = "Lsed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        author = "John Doe",
+        createdAt = Date(),
+        image = "https://picsum.photos/id/486/1280/720",
+        note = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Morbi tincidunt augue interdum velit euismod in pellentesque massa. Donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Tincidunt tortor aliquam nulla facilisi cras. Adipiscing at in tellus integer. Lorem donec massa sapien faucibus et molestie ac feugiat sed. Adipiscing elit ut aliquam purus sit amet luctus venenatis. Nisl nunc mi ipsum faucibus. A pellentesque sit amet porttitor. Mattis rhoncus urna neque viverra justo nec ultrices dui sapien.\\n\\nElementum nisi quis eleifend quam adipiscing vitae proin sagittis. Faucibus pulvinar elementum integer enim neque. Dapibus ultrices in iaculis nunc sed. Sit amet justo donec enim diam vulputate ut pharetra. Risus at ultrices mi tempus. Cursus in hac habitasse platea dictumst quisque sagittis purus sit. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Imperdiet sed euismod nisi porta lorem mollis aliquam ut. Diam maecenas ultricies mi eget. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis. Non diam phasellus vestibulum lorem sed risus ultricies tristique. In aliquam sem fringilla ut morbi tincidunt augue interdum. Lorem sed risus ultricies tristique nulla. Purus semper eget duis at tellus at urna condimentum. Feugiat vivamus at augue eget arcu dictum."
+    )
+    AppTheme(darkTheme = true) {
+        NoteDetailContent(note)
     }
 }
