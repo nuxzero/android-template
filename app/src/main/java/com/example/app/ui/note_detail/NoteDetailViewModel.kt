4dcdd8f524a1d7291d37fdfa5ab8e5a8e80283c1
Natthawut Haematulin <natthawut1991@gmail.com>
2021-08-28 22:38:33 +0700

    refactor: convert LiveData to StateFlow

--- app/src/main/java/com/example/app/ui/note_detail/NoteDetailViewModel.kt
+++ app/src/main/java/com/example/app/ui/note_detail/NoteDetailViewModel.kt
@@ -1,22 +1,17 @@
 package com.example.app.ui.note_detail
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asFlow
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.switchMap
 import com.example.app.data.NoteRepository
 import com.example.app.data.models.Note
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class NoteDetailViewModel @Inject constructor(private val repository: NoteRepository) : ViewModel() {
-    private val noteId = MutableLiveData<Int>()
-    val note: Flow<Note> = noteId.switchMap { id ->
-        repository.getNote(id).asLiveData()
-    }.asFlow()
+class NoteDetailViewModel @Inject constructor(repository: NoteRepository) : ViewModel() {
+    private val noteId = MutableStateFlow(-1)
+    val note: Flow<Note> = repository.getNote(noteId.value)
 
     fun setNoteId(id: Int) {
         noteId.value = id
